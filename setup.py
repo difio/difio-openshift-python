a45eac9737fe0fa91ebb0d2f89e31f11da0dac7f
Alexander Todorov <atodorov@otb.bg>
2012-01-17 13:50:48 +0200

    read long_description from file and update download url

--- setup.py
+++ setup.py
@@ -2,67 +2,35 @@
 
 from distutils.core import setup
 
+with open('README.txt') as file:
+    long_description = file.read()
+
+name = 'monupco-openshift-express-python'
+version='1.2'
+
 setup(
-    name='monupco-openshift-express-python',
-    version='1.2',
+    name=name,
+    version=version,
     description='monupco.com client for OpenShift Express / wsgi-3.2 applications',
     author='Alexander Todorov',
     author_email='atodorov@nospam.otb.bg',
     url='http://monupco.com/',
-    download_url='https://github.com/monupco/monupco-openshift-express-python/downloads',
+    download_url='http://pypi.python.org/packages/source/m/%s/%s-%s.tar.gz' % (name, name, version),
+#    source_url = 'http://github.com/monupco/monupco-openshift-express-python/',
     py_modules=['monupco-openshift-express-python'],
     keywords = ['openshift', 'monupco', 'updates', 'cloud'],
     classifiers = [
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Development Status :: 5 - Production",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
-        "Intended Audience :: System administrators",
+        "Intended Audience :: System Administrators",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
         "Topic :: System",
         "Topic :: System :: Monitoring",
-        "Topic :: System :: Systems administration",
+        "Topic :: System :: Systems Administration",
         ],
-    long_description = """
-monupco.com client for OpenShift Express / wsgi-3.2
---------------------------------------------------------
-
-This is a client side agent for monupco.com. It compiles
-a list of locally installed Python packages and sends it to
-monupco.com.
-
-
-Installing on your OpenShift application
-----------------------------------------
-
-Create an account at <http://monupco.com>
-
-Create a wsgi-3.2 application on OpenShift
-
-    rhc-create-app -a myapp -t wsgi-3.2
-
-Add a dependency in your setup.py file
-
-    from setuptools import setup
-
-    setup(
-        name='MyApplication',
-        version='1.0',
-        install_requires=['monupco-openshift-express-python'],
-     )
-
-Set your username and application name in the ./data/MONUPCO_SETTINGS file
-
-    echo "export MONUPCO_USERNAME='YourUserName'"  >  ./data/MONUPCO_SETTINGS
-    echo "export MONUPCO_APP_NAME='MyApplication'" >> ./data/MONUPCO_SETTINGS
-
-Then push your application to OpenShift
-
-    git push
-
-That's it, you can now checkout your application statistics at:
-<http://monupco.com>
-"""
+    long_description = long_description,
      )
